83ed1dc68d5bf51a08cb8371a821ee2210dffa94
philleer <phillee2016@163.com>
2019-09-10 21:41:55 +0800

    add the opencv link and compile-pass-tested

--- src/CacheFunction.cc
+++ src/CacheFunction.cc
@@ -8,9 +8,23 @@ void github::Option::Run()
 		for (int i = 0; i < len; i++) {
 			std::cout << config[i] << std::endl;
 		}
+		std::string base_path = "/home/phil/Downloads";
+		char path[100];
+		sprintf(path, "%s/%s", base_path.c_str(), "der_hass-20140923/IMG_0137.JPG");
+		cv::Mat img = cv::imread(path, 0);
+		int width = img.cols;
+		int height = img.rows;
+		std::cout << "height * width " << height << " " << width << std::endl;
 	} else {
 		std::cerr << "Nothing has been given for processing!" << std::endl;
 	}
+
+	std::string base_path = "/home/phil/Downloads/dino";
+	char img_path[100];
+	sprintf(img_path, "%s/%s.png", base_path.c_str(), "dino0100");
+	cv::Mat img = cv::imread(img_path, 0);
+	cv::imshow("dino_img", img);
+	cv::waitKey(0);
 	auto end = std::chrono::system_clock::now();
 	auto duration = std::chrono::duration<double>(end - start);
 	std::cout << "Cost time " << duration.count() << " s" << std::endl;
--- src/CacheFunction.h
+++ src/CacheFunction.h
@@ -5,6 +5,10 @@
 #include <string>
 #include <chrono>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 namespace github { // Begin of namespace github
 
 class Option
